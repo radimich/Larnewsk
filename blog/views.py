76e820426ceb17dcde0b9a52db1b7965efd102b0
Radimich <Radimich25@yandex.ru>
2020-06-04 17:24:41 +0300

    2 com

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpRespinse
+from django.http import HttpResponse
 
-def home():
+def home(request):
+    return HttpResponse('<h2>Hello</2>')
+
+def contacts(request):
+    return HttpResponse('<h2>Контакты</2>')
